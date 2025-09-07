de2d341b89aaa51488b113d576b4f84b52426f3f
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-09-07 13:39:46 +0900

    oauth 認可がちゃんと通った

--- src/routers/oauth/google.ts
+++ src/routers/oauth/google.ts
@@ -1,12 +1,16 @@
 import { Hono, Context } from 'hono';
 import { env } from 'hono/adapter';
-import crypto from "crypto";
+import crypto from 'crypto';
 import { GenerateAuthUrlOpts, OAuth2ClientOptions, OAuth2Client, Credentials } from 'google-auth-library';
 
 const googleRouter = new Hono();
 
 googleRouter.get('/', (c) => {
   return c.json({ hello: 'oauth google Router' });
+});
+
+googleRouter.get('/error', (c) => {
+  return c.text('OAuth Failed.');
 });
 
 googleRouter.get('/usercheck', (c) => {
@@ -23,14 +27,22 @@ googleRouter.get('/auth', (c) => {
   return c.redirect(oauth2Client.generateAuthUrl(oauthClientAuthUrlOptions));
 });
 
-googleRouter.get('/callback', (c) => {
-  return c.json({ hello: 'success' });
+googleRouter.get('/callback', async (c) => {
+  const oauth2Client = getOAuth2Client(c);
+  const authCode = c.req.query('code');
+  if (authCode) {
+    const results = await oauth2Client.getToken(authCode);
+    return c.json(results);
+  } else {
+    return c.redirect('/oauth/google/error');
+  }
 });
 
 function getOAuth2Client(context: Context): OAuth2Client {
   const envConfigs = env(context);
   const currentUrl = new URL(context.req.url);
   currentUrl.pathname = `/oauth/google/callback`;
+  currentUrl.search = '';
   const globalOauth2ClientSettings: OAuth2ClientOptions = {
     clientId: envConfigs.GOOGLE_API_OAUTH2_CLIENT_ID?.toString(),
     clientSecret: envConfigs.GOOGLE_API_OAUTH2_CLIENT_SECRET?.toString(),
