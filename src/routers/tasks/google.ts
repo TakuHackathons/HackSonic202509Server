bafa98b6db72f8f365598c62a1d1d14e88750aee
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-09-07 14:20:07 +0900

    完了したタスクの情報を取得できるように

--- src/routers/tasks/google.ts
+++ src/routers/tasks/google.ts
@@ -27,7 +27,21 @@ googleTasksRouter.get('/list', async (c) => {
   return c.json(allTasks);
 });
 
-googleTasksRouter.get('/completes', (c) => {
+googleTasksRouter.get('/completes', async (c) => {
+  // Show: https://developers.google.com/workspace/tasks/reference/rest/v1/tasks/list
+  const oauth2Client = new google.auth.OAuth2(getOauth2ClientSettings(c));
+  oauth2Client.setCredentials({
+    refresh_token: 'refresh_token',
+  });
+  const service = google.tasks({ version: 'v1', auth: oauth2Client });
+  const tasklistResponses = await service.tasklists.list();
+  const allTasks = [];
+  for (const taskListItem of tasklistResponses.data.items || []) {
+    const taskResponse = await service.tasks.list({ tasklist: taskListItem.id?.toString(), showCompleted: false, maxResults: 100 });
+    for (const taskItem of taskResponse.data.items || []) {
+      allTasks.push(taskItem);
+    }
+  }
   return c.json({ hello: 'tasks google Router' });
 });
 
